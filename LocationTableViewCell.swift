a2152fb3048f218f1307469275de5c94fc4e7ca6
Jonathan <jonthomas2011@gmail.com>
2016-05-10 14:53:12 -0400

    added tableViewController for locations to app

---
+++ LocationTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LocationTableViewCell.swift
+//  Weather App
+//
+//  Created by Jonathan Thomas on 5/10/16.
+//  Copyright © 2016 Jonathan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var TemperatureView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Weather App/LocationTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  LocationTableViewController.swift
+//  Weather App
+//
+//  Created by Jonathan Thomas on 5/10/16.
+//  Copyright © 2016 Jonathan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var locations = [Location]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // load sample data
+        loadSampleLocations()
+    }
+    
+    func loadSampleLocations(){
+        let location1 = Location(zipcode: 33813, Temperature: 75)!
+        let location2 = Location(zipcode: 32608, Temperature: 81)!
+        
+        locations += [location1, location2]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "LocationTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! LocationTableViewCell
+
+        let location = locations[indexPath.row]
+
+        cell.locationLabel.text = String(location.zipcode)
+      //  cell.TemperatureView
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
